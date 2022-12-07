1428d1dc990b1a174bbdeca6353c1070087a5c68
JoelWeberDev <116774885+JoelWeberDev@users.noreply.github.com>
2022-12-06 23:05:45 -0500

    File reformatting

--- JS/ImageCarasol.js
+++ JS/ImageCarasol.js
@@ -3,7 +3,8 @@ var images = ["Images\\PXL_20220911_185013174.jpg","Images\\PXL_20220911_1904460
 function toggleImg(dir) {
     const imgEl = document.getElementById("imgCont");
     let path = imgEl.getAttribute("src");
-    let index = images.indexOf(path);
+    let index = images.indexOf(path.replace('../../',''));
+    console.log(index)
     let newPath = images[((index + dir) > -1? (index + dir):3)%images.length];
-    imgEl.setAttribute("src",newPath);
+    imgEl.setAttribute("src",`../../${newPath}`);
 }
