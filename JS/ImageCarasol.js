1efaf73687181145b4b60eee623dc48b19140c86
JoelWeberDev <116774885+JoelWeberDev@users.noreply.github.com>
2023-09-26 21:27:42 -0400

    Major revisions
    Updated About page
    Changed resume

--- JS/ImageCarasol.js
+++ JS/ImageCarasol.js
@@ -1,4 +1,4 @@
-var images = ["Images\\PXL_20220911_185013174.jpg","Images\\PXL_20220911_190446050.jpg","Images\\PXL_20220911_190545120.jpg","Images\\PXL_20220911_185801600.jpg"];
+var images = ["Images\\PXL_20220911_184324939.PORTRAIT.jpg","Images\\PXL_20220911_190545120.jpg","Images\\PXL_20220911_185013174.jpg","Images\\PXL_20220911_184853895.jpg"];
 
 function toggleImg(dir) {
     const imgEl = document.getElementById("imgCont");
